e5e1469fb2679a9641a1eed1009c7d709bc73c46
Jhonny Goya <jj_kenji@hotmail.com>
2022-04-21 14:30:41 -0300

    coloring the helmet

--- scripts/login.js
+++ scripts/login.js
@@ -1,15 +1,7 @@
-/*Colorir o capacete....... quebrando a cabeça ainda*/
+/*Colorir o capacete*/
 let helmetColor = document.querySelector('#helmetColor');
 let helmetIcon = document.querySelector('.colorable');
-let testButton = document.querySelector('#testButton');
 
-testButton.addEventListener('click', () => {
-    document.getElementById('helmetIcon').style.color = helmetColor.value;
-})
-
-
-
-/*
 helmetColor.addEventListener('input', () => {
-  document.getElementById('helmetIcon').style.color = helmetColor.value;
-});*/
+  helmetIcon.style.background = helmetColor.value;    
+});
