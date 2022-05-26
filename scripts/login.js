d3b496a280fe51eb278b535356447f6a4a19b9bf
Jhonny Goya <jj_kenji@hotmail.com>
2022-05-26 19:13:29 -0300

    standard layout

--- scripts/login.js
+++ scripts/login.js
@@ -1,3 +1,16 @@
+// **** Change Logo image according to mouse presence ****
+let myImage = document.querySelector('#f1Logo');
+
+myImage.addEventListener('mouseover', () => {
+  let mySrc = myImage.getAttribute('src');
+  myImage.setAttribute('src','images/F1logo_2018-2.png');
+})
+
+myImage.addEventListener('mouseleave', () => {
+  let mySrc = myImage.getAttribute('src');
+  myImage.setAttribute('src','images/F1logo_2018.png');
+})
+
 /*Colorir o capacete*/
 let helmetColor = document.querySelector('#helmetColor');
 let helmetIcon = document.querySelector('.colorable');
@@ -7,3 +20,4 @@ helmetIcon.style.background = helmetColor.value;
 helmetColor.addEventListener('input', () => {
   helmetIcon.style.background = helmetColor.value;    
 });
+
